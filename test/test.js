810bc23f3167cdce36e90c3ea0a4cff83af0887b
Bryan Alexander <bryanalexander@me.com>
2015-09-24 09:48:50 -1000

    tests written

--- test/test.js
+++ test/test.js
@@ -1,3 +1,27 @@
 var expect = chai.expect;
 var should = chai.should();
 
+describe('bubbleSort', function() {
+
+  it('should be a function', function() {
+
+    bubbleSort.should.be.a('function');
+
+  });
+
+  it('should take in an array as an argument and have two properties: currentLowest and compareValue', function() {
+
+    bubbleSort.should.have.property('inputArray');
+    expect(inputArray).to.be.an('array');
+    bubbleSort.should.have.property('currentLowest');
+    bubbleSort.should.have.property('currentLowest');
+
+  });
+
+  it('return the array in sorted order', function() {
+
+    expect(bubbleSort([5,2,7,4])).to.deep.equal([2,4,5,7]);
+
+  });
+
+});
