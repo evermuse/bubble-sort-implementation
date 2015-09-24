7d546a98b0208bfcc590c3cb96c6a5d988f451d4
Bryan Alexander <bryanalexander@me.com>
2015-09-24 13:15:29 -1000

    created index.html & style.css pages. rendering of array completed on index.html completed

--- app.js
+++ app.js
@@ -1,26 +1,91 @@
-function bubbleSort(arr) {
+//create module
 
-  var swapped;
+var bubbleSortModule = (function() {
 
-  do {
+  var module = {
 
-    swapped = false;
+    setStage : _setStage,
+    bubbleSort : _bubbleSort,
+    init : _init
+
+  };
+
+  function _setStage(arr) {
+
+    //define container
+
+    var bubbleSortContainer = document.createElement('section');
+    document.body.appendChild(bubbleSortContainer);
+    bubbleSortContainer.id = 'bubbleSortContainer';
+
+    //section heading
+
+    var bubbleSortHeading = document.createElement('h1');
+    bubbleSortContainer.appendChild(bubbleSortHeading);
+    bubbleSortHeading.id = 'bubbleSortHeading';
+    bubbleSortHeading.innerHTML = 'Bubble Sort Visualization';
+
+    //define new HTML element bubbleSort-block
+
+    var BubbleSortBlock = Object.create(HTMLElement.prototype);
+    document.registerElement('bubblesort-block');
+
+    //instantiate the bubbleSort-block elements on the page for the length of the array and create the block text node from the array
 
     for (var i = 0; i < arr.length; i++) {
 
-      if (arr[i] > arr[i + 1]) {
-
-        var temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        swapped = true;
-
-      }
+      var bubbleSortBlock = document.createElement('bubblesort-block');
+      bubbleSortContainer.appendChild(bubbleSortBlock);
+      bubbleSortBlock.className = 'bubbleSortBlock';
+      bubbleSortBlock.innerHTML = arr[i];
 
     }
 
-  } while (swapped);
+  }
 
-  return arr;
+  //bubble sort logic
 
-}
+  function _bubbleSort(arr) {
+
+    var swapped;
+
+    do {
+
+      swapped = false;
+
+      for (var i = 0; i < arr.length; i++) {
+
+        if (arr[i] > arr[i + 1]) {
+
+          var temp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
+          swapped = true;
+
+        }
+
+      }
+
+    } while (swapped);
+
+    return arr;
+
+  }
+
+  function _init() {
+
+    _setStage([5,4,2,9,8,7,1,5,3,6]);
+
+    //_bubbleSort();
+
+  }
+
+  return module;
+
+})();
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  bubbleSortModule.init();
+
+});
