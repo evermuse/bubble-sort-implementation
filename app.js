36812b9e2dcc78b224a3317c234e6b661c9199be
Bryan Alexander <bryanalexander@me.com>
2015-09-24 19:32:28 -1000

    visualization complete! used removeChild & insertBefore alongside setAttribute(data-id), & setInterval to animate the nodes moving. finished with a merge button and some css to create a nice user interface

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ var bubbleSortModule = (function() {
 
   };
 
-  function _setStage(arr) {
+  var arr = [5,4,2,9,8,7,1,5,3,6];
+
+  function _setStage() {
 
     //define container
 
@@ -36,53 +38,104 @@ var bubbleSortModule = (function() {
 
       var bubbleSortBlock = document.createElement('bubblesort-block');
       bubbleSortContainer.appendChild(bubbleSortBlock);
+      bubbleSortBlock.dataset.id = i;
       bubbleSortBlock.className = 'bubbleSortBlock';
       bubbleSortBlock.innerHTML = arr[i];
 
     }
 
+    //button for end-user to start merge
+
+    var bubbleSortStartButton = document.createElement('button');
+    bubbleSortContainer.appendChild(bubbleSortStartButton);
+    bubbleSortStartButton.id = 'bubbleSortStartButton';
+    bubbleSortStartButton.innerHTML = 'Merge';
+    bubbleSortStartButton.addEventListener('click', _bubbleSort);
+
   }
 
   //bubble sort logic
 
-  function _bubbleSort(arr) {
+  function _bubbleSort() {
+
+    //the swapped var will let be used to know when the sort is done
 
     var swapped;
 
-    do {
+    //wrap the for loop in an interval so the movement of nodes is visible as the sort progresses
+
+    var toStop = setInterval(function() {
 
       swapped = false;
+
+      //comparison nodes to enable removal and insertion on the DOM
+
+      var currentNode;
+      var nodeAtNext;
+      var nodeToMove;
 
       for (var i = 0; i < arr.length; i++) {
 
         if (arr[i] > arr[i + 1]) {
 
+          //if the current value is greater than the next set a temp variable with the current value
+
           var temp = arr[i];
+
+          //similar to above set the current node and next node to enable DOM removal / insertion
+
+          currentNode = document.querySelector('bubblesort-block[data-id="' + i + '"]');
+          nodeAtNext = document.querySelector('bubblesort-block[data-id="' + [i + 1] + '"]');
+
+          //assign current value to the next index position
+
           arr[i] = arr[i + 1];
+
+          //remove the nextNode from the DOM and reset the data-id attribut to the correct index position
+
+          nodeToMove = bubbleSortContainer.removeChild(nodeAtNext);
+          nodeToMove.setAttribute('data-id', i);
+
+          //insert the removed node back into the DOM before the currentNode and set the currentNode's data-id to the nextNode
+
+          bubbleSortContainer.insertBefore(nodeToMove, currentNode);
+          currentNode.setAttribute('data-id', i + 1);
+
+          //re-assign the temp variable to the next array position
+
           arr[i + 1] = temp;
+
           swapped = true;
 
         }
 
       }
 
-    } while (swapped);
+      if (!swapped) {
+
+        clearInterval(toStop);
+
+      }
+
+    }, 500);
 
     return arr;
 
   }
 
+  //init function to set the stage
+
   function _init() {
 
-    _setStage([5,4,2,9,8,7,1,5,3,6]);
-
-    //_bubbleSort();
+    _setStage();
 
   }
 
   return module;
 
 })();
+
+//wait until the DOM is loaded to set the stage
 
 document.addEventListener('DOMContentLoaded', function() {
 
